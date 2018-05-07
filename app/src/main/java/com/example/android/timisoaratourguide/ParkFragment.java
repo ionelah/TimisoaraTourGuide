af5bf1ded5083fe18e870d54355c7d41ebd482e1
ionelah <35505964+ionelah@users.noreply.github.com>
2018-05-07 22:06:43 +0300

    finalized application

--- app/src/main/java/com/example/android/timisoaratourguide/ParkFragment.java
+++ app/src/main/java/com/example/android/timisoaratourguide/ParkFragment.java
@@ -28,9 +28,9 @@ public class ParkFragment extends Fragment {
 
        locations.add(new Location(R.drawable.parc1,getString(R.string.first_park_name),getString(R.string.first_park_description),getString(R.string.first_park_address)));
        locations.add(new Location(R.drawable.parc2,getString(R.string.second_park_name),getString(R.string.second_park_description),getString(R.string.second_park_address)));
-       locations.add(new Location(R.drawable.parc2,getString(R.string.third_park_name),getString(R.string.third_park_description),getString(R.string.thirds_park_address)));
-       locations.add(new Location(R.drawable.parc2,getString(R.string.fourth_park_name),getString(R.string.fourth_park_description),getString(R.string.fourth_park_address)));
-       locations.add(new Location(R.drawable.parc2,getString(R.string.fith_park_name),getString(R.string.fifth_park_description),getString(R.string.fifth_park_address)));
+       locations.add(new Location(R.drawable.parc3,getString(R.string.third_park_name),getString(R.string.third_park_description),getString(R.string.thirds_park_address)));
+       locations.add(new Location(R.drawable.parc4,getString(R.string.fourth_park_name),getString(R.string.fourth_park_description),getString(R.string.fourth_park_address)));
+       locations.add(new Location(R.drawable.parc5,getString(R.string.fith_park_name),getString(R.string.fifth_park_description),getString(R.string.fifth_park_address)));
 
        //Create an ArrayAdapter whose data source is a list of strings.
         LocationAdapter adapter = new LocationAdapter(getContext(), locations);
